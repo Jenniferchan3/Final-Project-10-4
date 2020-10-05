2ed55b7c7d485bb23f79aa937306e3dbe25bf1bf
Jenniferchan3 <68825434+Jenniferchan3@users.noreply.github.com>
2020-10-05 19:56:55 -0400

    updated js, and html css

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
 const searchButton = document.getElementById("search-button");
 const closeModal = document.getElementById("close-button");
 const submitButton = document.getElementById("submit-button");
+const allston = document.getElementById("allston");
+const ctown = document.getElementById("ctown");
+const cambridge = document.getElementById("cambridge");
+const teashopOnly = document.getElementById("tea-shop-only");
+const cafe = document.getElementById("cafe");
+const dessert = document.getElementById("dessert");
+const tea = document.getElementById("tea");
+const cheese = document.getElementById("cheese");
+const fruitshakes = document.getElementById("fruitshakes");
 
 searchButton.addEventListener("click", function () {
   document.getElementById("modal-main").classList.add("open");
-  // searchButton.innerHTML = "";
 });
 
 closeModal.addEventListener("click", function () {
@@ -12,25 +20,41 @@ closeModal.addEventListener("click", function () {
 });
 
 submitButton.addEventListener("click", function () {
-  document.getElementById("results-text").classList.toggle("open");
-  document.getElementById("modal-text").classList.toggle("hide");
-  document.body.style.backgroundImage = "url(../img/bobafriends-02.jpg)";
+  document.getElementById("results-text").classList.add("open");
+  document.getElementById("modal-text").classList.add("hide");
+  document.body.style.backgroundImage = "url(../img/bobafriends-01.jpg)";
 
   if (
     allston.checked === true &&
-    cafe.checked === true &&
+    teashopOnly.checked === true &&
     tea.checked === true
   ) {
+    console.log("first ones are checked");
     document.getElementById("option-1").classList.remove("hide");
   }
-  // else if (
-  //   backbay.checked === true &&
-  //   teaShopOnly.checked === true &&
-  //   withCheeseFoam.checked === true &&
-  // ) {
-  // remove hide class from option-2
-  // }
-  // else if for option 3
+  else if (
+    ctown.checked === true &&
+    teaShopOnly.checked === true &&
+    tea.checked === true &&
+  ) {
+    document.getElementById("option-2").classList.remove("hide");
+  }
+  else if (
+    cambridge.checked === true &&
+    teaShopOnly.checked === true &&
+    tea.checked === true &&
+  ) {
+    document.getElementById("option-3").classList.remove("hide");
+  }
+});
+
+
+
+
+
+// searchButton.addEventListener("click", function () {
+//   document.getElementById("modal-main").reset();
+
+  // else (
   // if (nearbyArea === allston && foodChoices === cafe && typesTea === teaonly) {
   // }
-});
